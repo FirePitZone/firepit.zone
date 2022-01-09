23bc08d073aabb319199f74cd83ab05db4e7e27a
Andrew Gleeson <heygleeson@gmail.com>
2022-01-09 22:01:17 +1100

    Fixed node server.

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const PORT = 3000;
 
-const hostname  = '127.0.0.1';
-const port      = 3000;
+const fs = require("fs");
+const express = require("express");
+const expressApp = express();
+expressApp.use(express.static("./")); // Serve Static Files
+const httpServer = require("http").createServer(expressApp);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+httpServer.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}!`);
 });
